2ed409c02bad0035e716f53c4433952fdb4d57e6
garza <garza@cjas.org>
2017-10-11 21:32:54 -0500

    making transform.sh easier to read

--- xslt/transform.sh
+++ xslt/transform.sh
@@ -1,1 +1,2 @@
-docker run -it --rm=true -v $(pwd)/src:/home rafagc/saxonb saxonb-xslt -s:xml/flex.xml -xsl:xslt/flex.xsl -o:html/flex.html
+docker run -it --rm=true -v $(pwd)/src:/home rafagc/saxonb \
+saxonb-xslt -s:xml/flex.xml -xsl:xslt/flex.xsl -o:html/flex.html
