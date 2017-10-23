74a5dbc92b55008eff34ddfc2b2ccffd78d30c2d
garza <garza@cjas.org>
2017-10-22 20:16:48 -0500

    reorganizing into demo folders

--- mysql/mysql.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-## docker run -it, same as demo 1
-## --rm             :  make sure we remove the container when we are done
-## -p 3306          :  for now, we also bind our hosts's 3306 port to the container's
-##                     so we can connect to mysql from the host, if needed
-## my-mysql         :  the name of the container given in the docker build step
-
-docker run -it --rm -p 3306:3306 -v $(pwd)/mysql-data:/var/lib/mysql my-mysql
